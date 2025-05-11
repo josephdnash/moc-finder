ebe93b75074e45e70bacef1558584ce7d89657c2
josephdnash <107000388+josephdnash@users.noreply.github.com>
2025-05-10 19:51:42 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const Roboto_Mono = Roboto({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
